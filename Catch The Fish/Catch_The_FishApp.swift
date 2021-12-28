aa9b3bbc7898eb2f2ec1f586f4b41c2d63b3cd98
nureddinelmas <nureddinelmas34@gmail.com>
2021-12-28 12:05:47 +0100

    Initial Commit

---
+++ Catch The Fish/Catch_The_FishApp.swift
@@ -0,0 +1,17 @@
+//
+//  Catch_The_FishApp.swift
+//  Catch The Fish
+//
+//  Created by Nureddin Elmas on 2021-12-28.
+//
+
+import SwiftUI
+
+@main
+struct Catch_The_FishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
